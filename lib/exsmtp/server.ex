cb36d6dc93a5903543777c4ed9bfd00b765d70f6
tovam <tovam@users.noreply.github.com>
2020-04-10 19:43:00 +0200

    Update server.ex

--- lib/exsmtp/server.ex
+++ lib/exsmtp/server.ex
@@ -2,8 +2,8 @@ defmodule Exsmtp.Server do
   def start_link do
     session_options = [callbackoptions: [parse: true]]
     smtp_port = Application.get_env(:exsmtp, :port) #config(:smtp_port)
-    smtp_server_options = [[port: smtp_port,
-                            sessionoptions: session_options]]
+    smtp_server_options = [port: smtp_port,
+                            sessionoptions: session_options]
     :gen_smtp_server.start(Exsmtp.Handler,
                            smtp_server_options)
   end
