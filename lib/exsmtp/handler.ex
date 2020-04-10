684ebb344bf61a8b07fe8e6d9644d08ae03d7a2b
tovam <tovam@users.noreply.github.com>
2020-04-11 01:53:06 +0200

    Update handler.ex

--- lib/exsmtp/handler.ex
+++ lib/exsmtp/handler.ex
@@ -19,31 +19,38 @@ defmodule Exsmtp.Handler do
   end
 
   def handle_HELO(hostname, state) do
-    IO.puts("#{@smtp_requested_action_okay} HELO from #{hostname}")
+    Logger.debug(" EXSMTP/handle_HELO")
+    Logger.debug("#{@smtp_requested_action_okay} HELO from #{hostname}")
     {:ok, 655360, state}
   end
 
   def handle_EHLO(_hostname, extensions, state) do
+    Logger.debug(" EXSMTP/handle_EHLO")
     {:ok, extensions, state}
   end
 
   def handle_MAIL(_sender, state) do
+    Logger.debug(" EXSMTP/handle_MAIL")
     {:ok, state}
   end
 
   def handle_RCPT(_to, state) do
+    Logger.debug(" EXSMTP/handle_RCPT")
     {:ok, state}
   end
 
   def handle_VRFY(_address, state) do
+    Logger.debug(" EXSMTP/handle_VRFY")
     {:error, "252 VRFY disabled by policy, no peeking", state}
   end
 
   def handle_DATA(_from, _to, "", state) do
+    Logger.debug(" EXSMTP/handle_DATA")
     {:error, "#{@smtp_mail_action_abort} Message too small", state}
   end
 
   def handle_DATA(from, to, data, state) do
+    Logger.debug(" EXSMTP/handle_DATA")
     unique_id = UUID.uuid4()
     Logger.debug("Message from #{from} to #{to} with body length #{byte_size(data)} queued as #{unique_id}")
     mail = parse_mail(data, state, unique_id)
@@ -52,18 +59,22 @@ defmodule Exsmtp.Handler do
   end
 
   def handle_MAIL_extension(extension, _state) do
+    Logger.debug(" EXSMTP/handle_MAIL_extension")
     {:error, "Unknown MAIL FROM #{extension}"}
   end
 
   def handle_RCPT_extension(extension, _state) do
+    Logger.debug(" EXSMTP/handle_RCPT_extension")
     {:error, "Unknown RCPT TO extension #{extension}"}
   end
 
   def handle_RSET(state) do
+    Logger.debug(" EXSMTP/handle_RSET")
     state
   end
 
   def handle_other(verb, _args, state) do
+    Logger.debug(" EXSMTP/handle_other")
     {["#{@smtp_unrecognized_command} Error: command not recognized : '", verb, "'"], state}
   end
 
@@ -76,6 +87,7 @@ defmodule Exsmtp.Handler do
   end
 
   defp parse_mail(data, _state, _unique_id) do
+    Logger.debug(" EXSMTP/parse_mail")
     try do
       :mimemail.decode(data)
     rescue
